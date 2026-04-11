d5351d9b6dec8938689be6a91d9ae35767c78eb0
Benni <benimin574@gmail.com>
2026-04-11 21:18:33 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: "127.0.0.1",
     port: 5173,
+    allowedHosts: [".trycloudflare.com"],
     proxy: {
       "/matchmake": {
         target: "http://127.0.0.1:2567",
